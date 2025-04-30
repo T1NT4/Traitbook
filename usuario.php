ef275f9ea7923fb4b74a4da8394593f6c031ad5c
T1NT4 <thiagogabriel.osil@gmail.com>
2025-04-30 15:46:07 -0300

    movi a pagina de usuario e tambem fiz alguns divs só aparecer se voce tiver alguma coisa naquele campo

--- usuario.php
+++ usuario.php
@@ -1,52 +1,206 @@
 <?php
-include_once __DIR__ . "/Controller/LoginController.php";
-include_once __DIR__ . "/Controller/ApiController.php";
-include_once __DIR__ . "/config.php";
+include_once __DIR__."/Controller/LoginController.php";
+include_once __DIR__."/Controller/ApiController.php";
+include_once __DIR__."/config.php";
 
-$Controller = new LoginController($pdo);
-if (!isset($_COOKIE['id_user'])) {
-    header("Location: index.php");
+if(!isset($_COOKIE['id_user'])){
+    header("Location: login.php");
 }
-$id_user = $_COOKIE['id_user'];
-$user = $Controller->listarContaPorID($id_user);
-
 $ApiController = new ApiController();
-if ($user['link_personalidade'] != null){
-    $personalityData = $ApiController->getDataFrom16PersonalitiesLink($user['link_personalidade']);
+$LoginController = new LoginController($pdo);
+
+$user = $LoginController->listarContaPorID($_COOKIE['id_user']);
+
+
+
+if ($user['link_personalidade'] == ""){
+    $personalidade = null;    
+}else{
+    $personalidade = $ApiController->getDataFrom16PersonalitiesLink($user['link_personalidade']);
+    $jsonAnim = file_get_contents($personalidade['avatar']['staticBodyJson']);
+    $svgData = file_get_contents($personalidade['avatar']['staticBodyPath']);
+
 }
+
+
+$genero_pt = [
+    'Female' => 'Mulher',
+    'Male' => 'Homem',
+    'Other' => 'Outro'
+];
+$aniversario = new DateTime($user['aniversario']);
+$cores = [
+    'blue' => '#4297B3',
+    'yellow' => '#E4AE3A',
+    'green' => '#33A474',
+    'purple' => '#88619A',
+    'red' => '#F25E62'
+];
+$tem_pontos_fracos_ou_fortes = ($user['pontos_fortes'] != null) OR ($user['pontos_fortes'] != '') OR ($user['pontos_fracos'] != null) OR ($user['pontos_fracos'] != '');
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="Captura_de_tela_2024-11-11_140326-removebg-preview (1).png" type="image/png">
-    <link rel="stylesheet" href="estilo.css">
-    <title>Página de perfil</title>
+    <title>Usuário</title>
+    <link rel="stylesheet" href="View/style.css">
 </head>
-
 <body>
-    <div class="seila">
-        <div class="texto">
-            <img width="400" src="View/fotos_de_perfil/<?= $user['nome_arquivo_fotoperfil'] ?>"
-                alt="foto de perfil de <?= $user['username'] ?>">
-            <p><b>Nome de usuário: </b><?= $user['username'] ?></p>
+    <header>
+        <div class="logo">
+            <img src="View/imgs/Logo-Traitbook.svg">
         </div>
-        <div class="navnav">
-            <a href="user-actions/mudar-foto-perfil.php">Mudar foto de perfil</a>
-            <a href="user-actions/mudar-senha.php">Mudar senha</a>
-            <a href="user-actions/logout.php">Sair da conta</a>
-            <a href="teste-de-personalidade.php">Teste de personalidade</a>
+        <div class="pfp mini">
+            <img src="View/fotos_de_perfil/<?=$user['nome_arquivo_fotoperfil']?>">
         </div>
-        <?php if(isset($personalityData)):?>
-            <div>
-                <p><b>Personalidade:</b><?=$personalityData['personalityShort']?></p>
-                <img src="<?=$personalityData['details']['cards']['personality']['imageSrc']?>" alt="<?=$personalityData['details']['cards']['personality']['imageAlt']?>" width="200">        
+    </header>
+    <section>
+        <div class="flex-row height-400 flex-wrap-at-760">
+            <div class="glass width-150po flex-row align-start ">
+                <div class="pfp">
+                    <img src="View/fotos_de_perfil/<?=$user['nome_arquivo_fotoperfil']?>">
+                </div>
+                <br>
+                <div class="flex-column nogap grow-100">
+                    <h3>Nome de usuário:</h3>
+                    <h1><?=$user['username']?></h1>
+                    <br>
+                    <h3>Nome:</h3>
+                    <h1><?=$user['nome_inteiro']?></h1>
+                    <br>
+                    <h3>Email:</h3>
+                    <h1><?=$user['email']?></h1>
+                    <br>
+                    <h3>Genêro:</h3>
+                    <h1><?=$genero_pt[$user['genero']]?></h1>
+                    <br>
+                    <h3>Data de nascimento:</h3>
+                    <h1><?=$aniversario->format('d/m/Y')?></h1>
+                    <br>
+                    <a class="button glass self-align-center" href="edit_perfil.php">
+                        <h1>Editar Perfil</h1>
+                    </a>
+                </div>
+            </div>
+            <?php if(!(($user['sobre_mim'] == null) OR ($user['sobre_mim'] == ''))):?>
+                <div class="glass flex-column width-100po">
+                    <h1>Sobre mim:</h1>
+                    <p>
+                        <?=$user['sobre_mim']?>
+                    </p>
+                </div>
+            <?php endif;?>
+        </div>
+        <?php if($tem_pontos_fracos_ou_fortes):?>
+            <div class="flex-row height-200 flex-wrap-at-760">
+                <div class="glass width-100po flex-column">
+                    <h1>Pontos fortes:</h1>
+                    <h3>-Handsome</h3>
+                    <h3>-Handsome</h3>
+                    <h3>-Handsome</h3>
+                    <h3>-Handsome</h3>
+                </div>
+                <div class="glass width-100po flex-column">
+                    <h1>Pontos fracos:</h1>
+                    <h3>-Só apareco por 1 minuto no jogo</h3>
+                </div>
             </div>
         <?php endif;?>
+        <div class="flex-row height-500 flex-wrap-at-1000">
+            <?php if((!(($user['profissao_atual'] == null) OR ($user['profissao_atual'] == ''))) OR
+                     (!(($user['minhas_aspiracoes'] == null) OR ($user['minhas_aspiracoes'] == ''))) OR
+                     (!(($user['meus_principais_objetivos'] == null) OR ($user['meus_principais_objetivos'] == '')))):?>
+            <div class="width-100po flex-column">
+                <?php if(!(($user['profissao_atual'] == null) OR ($user['profissao_atual'] == ''))):?>
+                <div class="glass grow-100 flex-column">
+                    <h1>Profissão atual:</h1>
+                    <p><?=$user['profissao_atual']?></p>
+                </div>
+                <?php endif;?>
+                <?php if(!(($user['minhas_aspiracoes'] == null) OR ($user['minhas_aspiracoes'] == ''))):?>
+                <div class="glass grow-100 flex-column">
+                    <h1>Minhas aspirações:</h1>
+                    <p><?=$user['minhas_aspiracoes']?></p>
+                </div>
+                <?php endif;?>
+                <?php if(!(($user['meus_principais_objetivos'] == null) OR ($user['meus_principais_objetivos'] == ''))):?>
+                <div class="glass grow-100 flex-column">
+                    <h1>Meus principais objetivos:</h1>
+                    <p><?=$user['meus_principais_objetivos']?></p>
+                </div>
+                <?php endif;?>
+            </div>
+            <?php endif;?>
+            <?php if($personalidade != null):?>
+                <div class="glass width-200po flex-row flex-wrap-at-760">
+                    <div class="flex-column width-100po">
+                        <h1>Personalidade: <?=$personalidade['personalityShort']?></h1>
+                        <div id="lottie-animation" style="width:400px; max-width: 80vw;">
+                            <?=$svgData?>
+                        </div>
+                        <a target="_blank" href="<?=$user['link_personalidade']?>">Link da personalidade no site oficial da 16Personalities</a>
+                        <a target="_blank" href="<?=$personalidade['personalityLink']?>">Saiba mais sobre essa personalidade no site 16Personalities</a>
+                    </div>
+                    <div class="flex-column width-150po">
+                        <div class="flex-column grow-100 width-100po" style="justify-content: center; gap:30px">
+                            <?php foreach($personalidade['details']['traits'] as $trait):?>
+                                <div class="flex-column nogap">
+                                    <h3><?=$trait['pct']. "% " . $trait['trait']?></h3>
+                                    <div class="glass width-100po height-15 flex-column nopadding overflow-h">
+                                        <?php if($trait['reversed'] == 1):?>
+                                            <div class="height-100po" style="width: <?=$trait['pct']?>%; background-color: <?=$cores[$trait['color']]?>;"></div>
+                                        <?php else: $reversepct = ($trait['pct']-100)*-1?>
+                                            <div class="height-100po" style="width: <?=$reversepct?>%; background-color: <?=$cores[$trait['color']]?>;"></div>
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="flex-row">
+                                        <p><?=$trait['titles'][0]?></p>
+                                        <p class="grow-100" style="text-align: right;"><?=$trait['titles'][1]?></p>
+                                    </div>
+                                </div>
+                            <?php endforeach;?>
+                            <a class="button glass self-align-center" href="teste-de-personalidade.php">
+                                <h1>Refazer teste de personalidade</h1>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php else:?>
+                <div class="glass width-100po justify-center flex-row">
+                    <a class="button glass self-align-center" href="../../teste-de-personalidade.php">
+                        <h1>Fazer o teste de personalidade</h1>
+                    </a>
+                </div>
+            <?php endif;?>
+        </div>
+    </section>
+    <div class="background" id="background">
+        <div class="background-img"></div>
+        <div class="background-img"></div>
     </div>
     
 </body>
-
 </html>
+<script>
+    var body = document.body;
+    var html = document.documentElement;
+
+    var height = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+    );
+
+    document.getElementById('background').style.height = height.toString()+"px"
+
+</script>    
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.7.5/lottie.min.js"></script>
+<script src="handleLottieAnim.js"></script>
+<script>
+    initAnim('<?=$jsonAnim?>');
+</script>
